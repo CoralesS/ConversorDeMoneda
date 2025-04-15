fbf5fe35fe00a490051dddd1689a9dce972c6de3
Dilver Corales <dilvercorales2004@gmail.com>
2025-04-15 18:11:04 -0500

    Primeraprueba

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int numero = 5;
+
+        Double  entero = (int) numero*3.4;
+
+        System.out.println(entero);
     }
 }
